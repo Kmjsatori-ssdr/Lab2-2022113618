b19af1f604eee8d40d2dda8cab15b9c76a3a151c
Selakz <14486837+selakz@user.noreply.gitee.com>
2024-11-05 10:58:11 +0800

    chore: add annotation

--- L2022113618_11_Test.java
+++ L2022113618_11_Test.java
@@ -4,7 +4,18 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 测试用例设计原则：等价类划分
+ * 分为三种情况：
+ * 1. 有多个解，且可以存在重复的解；
+ * 2. 仅有一个解；
+ * 3. 没有解。
+ */
 public class L2022113618_11_Test {
+    /**
+     * 测试目的：测试能否找出多个解，且不重复。
+     * 测试用例：[-1,0,1,2,-1,4]
+     */
     @Test
     public void testThreeSum1() {
         int[] nums = new int[]{-1, 0, 1, 2, -1, 4};
@@ -22,6 +33,10 @@ public class L2022113618_11_Test {
         Assert.assertEquals(trueRes, Solution.threeSum(nums));
     }
 
+    /**
+     * 测试目的：测试能否在没有解的情况下正确得到结果
+     * 测试用例：[0,1,1]
+     */
     @Test
     public void testThreeSum2() {
         int[] nums = new int[]{0, 1, 1};
@@ -29,6 +44,10 @@ public class L2022113618_11_Test {
         Assert.assertEquals(trueRes, Solution.threeSum(nums));
     }
 
+    /**
+     * 测试目的：测试能否在仅有单个解的情况下正确得到结果
+     * 测试用例：[0,0,0]
+     */
     @Test
     public void testThreeSum3() {
         int[] nums = new int[]{0, 0, 0};
